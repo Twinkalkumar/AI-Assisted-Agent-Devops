d127b9d7565bca439fa5d88572138abe3208125d
Twinkalkumar.patel <twinkalkumar.patel@civica.com>
2026-09-20 19:31:17 +0530

    updated missing operator

--- generate_dockerfile.py
+++ generate_dockerfile.py
@@ -14,7 +14,7 @@ def generate_dockerfile(language):
     response = ollama.chat(model='llama3.2:1b',message=[{'role': 'user', 'content': PROMPT.format(language=language)}])
     return response['message']['content']
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     language = input("Enter ther programming langauge:")
     dockerfile = generate_dockerfile(language)
     print("\nGenerated Dockerfile:\n")
