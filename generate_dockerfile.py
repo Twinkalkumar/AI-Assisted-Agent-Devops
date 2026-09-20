9747ddee4b988f5a1eee961a7f81604ae053837b
Twinkalkumar.patel <twinkalkumar.patel@civica.com>
2026-09-20 18:55:22 +0530

    added python file

---
+++ generate_dockerfile.py
@@ -0,0 +1,21 @@
+import ollama
+
+PROMT = """
+Only Generate an ideal dockerfile for {language} with best prectices. Do not provide any description
+include:
+- base image 
+- installing dependencies
+- setting working directory
+- adding source code
+- running the application
+"""
+
+def generate_dockerfile(language):
+    response = ollama.chat(model='llama3.2:1b',message=[{'role': 'user', 'content': PROMPT.format(language=language)}])
+    return response['message']['content']
+
+if __name__ = '__main__':
+    language = input("Enter ther programming langauge:")
+    dockerfile = generate_dockerfile(language)
+    print("\nGenerated Dockerfile:\n")
+    print(dockerfile)
